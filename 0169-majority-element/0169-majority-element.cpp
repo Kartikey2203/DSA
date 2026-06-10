2388c4b1c782b0389b8fccd7dffbcdbd0f967cf1
Kartikey Gupta <145404457+Kartikey2203@users.noreply.github.com>
2026-06-10 09:33:43 +0530

    Time: 1712 ms (5.02%), Space: 28.1 MB (86.85%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+     int size=nums.size();
+       for(int i=0; i<size; i++){
+        int count=0;
+         for(int j=0; j<size; j++){
+            if(nums[i]==nums[j]){
+                count ++;
+                if(count >size/2){
+                    return nums[i];
+                }
+            }
+         }
+       }
+       return 0;
+    }
+};
