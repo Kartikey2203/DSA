58d1d8ae4393ed908eba118e12bbe0421218e406
Kartikey Gupta <145404457+Kartikey2203@users.noreply.github.com>
2026-06-11 12:03:47 +0530

    Time: 5 ms (64.9%), Space: 128.4 MB (54.44%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -2,30 +2,19 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
         int n=nums.size();
-        vector<int> left(n/2);
-        vector<int> right(n/2);
-        int j=0,k=0;
+        vector<int> ans(n,0);
+        int j=0,k=1;
         for(int i=0; i<n; i++){
             if(nums[i]>0){
-                right[j]=nums[i];
-                j++;
+                ans[j]=nums[i];
+                j+=2;
             }
             else{
-                left[k]=nums[i];
-                k++;
+                ans[k]=nums[i];
+                k+=2;
             }
         }
-         j=0,k=0;
-        for(int i=0; i<n; i++){
-            if(i%2){
-                nums[i]=left[j];
-                j++;
-            }
-            else{
-                nums[i]=right[k];
-                k++;
-            }
-        }
-        return nums;
+       
+        return ans;
     }
 };
