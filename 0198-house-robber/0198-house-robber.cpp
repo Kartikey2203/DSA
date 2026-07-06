5efa00cf169465e35ee5968d8800958fda786302
Kartikey Gupta <145404457+Kartikey2203@users.noreply.github.com>
2026-07-06 13:49:07 +0530

    Time: 0 ms (100%), Space: 10.7 MB (23.11%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     int sol_mem(vector<int>& nums,vector<int>& dp,int i){
-        if(i>=nums.size()){
-            return 0;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
+        // if(i>=nums.size()){
+        //     return 0;
+        // }
+        // if(dp[i]!=0){
+        //     return dp[i];
+        // }
         int ans=0;
-        
+        // dp[i-1]=nums[i-1];
+        // dp[1]=nums[1];
         // if(i+1<nums.size())
-         ans=max(nums[i]+sol_mem(nums,dp,i+2),sol_mem(nums,dp,i+1));
-         dp[i]=ans;
-        return ans;
+        for(int j=i-1; j>=0; j--){
+         dp[j]=max(nums[j]+dp[j+2],dp[j+1]);
+        }
+        return dp[0];
     }
     int rob(vector<int>& nums) {
         int ans=0;
         int n=nums.size();
-        vector<int>dp(n+1,-1);
+        vector<int>dp(n+2,0);
 
-        ans=sol_mem(nums,dp,0);
+        ans=sol_mem(nums,dp,n);
         return ans;
     }
 };
