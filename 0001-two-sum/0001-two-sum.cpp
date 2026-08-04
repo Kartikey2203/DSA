3f204af1cbeaa3176fc88931eb69b228f88ec5c7
Kartikey Gupta <145404457+Kartikey2203@users.noreply.github.com>
2026-08-04 11:09:42 +0530

    Time: 6 ms (41.2%), Space: 14.9 MB (22.69%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int>mp;
-
+        unordered_map<int,int> mp;
         for(int i=0; i<nums.size(); i++){
-            int rest = target - nums[i];
-
-            if(mp.find(rest) != mp.end()){
-                return {mp[rest],i};
+            int temp=target-nums[i];
+            if(mp.find(temp)!=mp.end()){
+                return {i,mp[temp]};
             }
-
-            // Insert into hashmap 
-            mp[nums[i]] = i;
+            mp[nums[i]]=i;
         }
-
         return {};
+
     }
 };
