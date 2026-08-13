99e700972b9f349c5e7639c9e201794b99ac7144
Kartikey Gupta <145404457+Kartikey2203@users.noreply.github.com>
2026-08-13 23:10:55 +0530

    Time: 0 ms (100%), Space: 9 MB (37.95%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        int temp ;
+        for(auto ch: s){
+            if(ch=='('||ch=='['||ch=='{'){
+                st.push(ch);
+            }
+            else{
+                if(st.empty())
+                    return false;
+                else    
+                if(ch=='}' && st.top() =='{')
+                    st.pop();
+                else
+                if(ch==']' && st.top() =='[')
+                    st.pop();
+                else
+                if(ch==')' && st.top() =='(')
+                    st.pop();
+                else
+                    return false;
+            }
+        }
+        if(st.empty())
+            return true;
+            return false;
+    }
+};
