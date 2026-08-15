988889794b958b65faff8db21a943692a6f3c078
Kartikey Gupta <145404457+Kartikey2203@users.noreply.github.com>
2026-08-15 16:58:11 +0530

    Time: 0 ms (100%), Space: 9.9 MB (60.36%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
+void rec(vector<int>& nums,vector<vector<int>>& ans,int ind,vector<int>& temp){
+ans.push_back(temp);
+ for(int i=ind; i<nums.size(); i++){
+    temp.push_back(nums[i]);
+    rec(nums,ans,i+1,temp);
+    temp.pop_back();
+ }
+}
     vector<vector<int>> subsets(vector<int>& nums) {
-         vector<vector<int>> ans;
-         int temp=1<< nums.size();
-         for(int mask=0; mask<temp; mask++){
-            vector<int> subset;
-            for(int i=0; i<nums.size(); i++){
-                if(mask & (1<<i)){
-                    subset.push_back(nums[i]);
-                }
-            }
-            ans.push_back(subset);
-         }
-            return ans;
+        vector<vector<int>> ans;
+        vector<int> temp;
+        rec(nums,ans,0,temp);
+        return ans;
     }
 };
