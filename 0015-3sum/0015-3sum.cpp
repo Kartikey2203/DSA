b9e4e2859f6d484ea35b0a2d37cda414144dbc5c
Kartikey Gupta <145404457+Kartikey2203@users.noreply.github.com>
2026-08-15 10:05:32 +0530

    Time: 43 ms (83.5%), Space: 29.1 MB (73.36%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,81 +1,28 @@
-// class Solution {
-// public:
-//     vector<vector<int>> threeSum(vector<int>& nums) {
-//      vector<vector<int>> ans;
-//      for(int i=0; i<nums.size(); i++){
-//      for(int j=i+1; j<nums.size(); j++){
-//      for(int k=j+1; k<nums.size(); k++){
-//         // if(i!=j && j!=k && k!=i){
-//         int temp=nums[i]+nums[j]+nums[k];
-//         if(temp==0){
-//             vector<int> triplet={nums[i],nums[j],nums[k]};
-//             sort(triplet.begin(),triplet.end());
-//             if(find(ans.begin(),ans.end(),triplet)== ans.end())
-//             ans.push_back(triplet);
-//             // cout<<i<<j<<k;
-//         }
-//         }
-//     //  }
-//      }
-//      }
-//        return ans;
-//     }
-// };
-// class Solution {
-// public:
-//     vector<vector<int>> threeSum(vector<int>& nums) {
-    //     //  ans;
-    //     sort(nums.begin(), nums.end()); // Step 1: sort
-    //     int n = nums.size();
-    //     set<vector<int>> un;
-
-    //     for (int i = 0; i < n; i++) {
-    //       int tar=-nums[i];
-    //       set<int> s;
-    //       for(int j=i+1; j<n; j++){
-    //       int c=tar-nums[j];
-    //       if(s.find(c)!=s.end()){
-    //        vector<int> trip={nums[i],nums[j],c};
-    //        un.insert(trip);
-    //       }
-    //       s.insert(nums[j]);
-    //     }
-    //     }
-    //    vector<vector<int>> ans(un.begin(),un.end());
-    //     return ans;
-    // }
-// };
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n=nums.size(); 
-        vector<vector<int>> ans;
-            vector<int> temp;
+        int n=nums.size();
         sort(nums.begin(),nums.end());
-        for(int i=0; i<n; i++){
-            int j=i+1,k=n-1;
+        vector<vector<int>> ans;
+        for(int i=0; i<n-2; i++){
+            int j=i+1,k=n-1; 
             if(i>0 && nums[i]==nums[i-1]) continue;
             while(j<k){
-            int tar=nums[i]+nums[j]+nums[k];
-            if(tar<0){
-                j++;
-            }
-            else 
-            if(tar>0){
-                k--;
-            }
-            else{
-                vector<int> temp={nums[i],nums[j],nums[k]};
-                 ans.push_back(temp);
+                int s=nums[i]+nums[j]+nums[k];
+                if(s>0) k--;
+                else
+                if(s<0) j++;
+                else{
+                ans.push_back({nums[i],nums[j],nums[k]});
                 j++;
                 k--;
-            while(j<k && nums[j]==nums[j-1]){
+                while(j<n-1 && nums[j]==nums[j-1]){
                 j++;
+                 continue;
+                }
+                }
             }
-            }
-            }
-              
         }
         return ans;
     }
-    };
+};
