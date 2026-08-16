cd06668fd3585a7af0630336b383b1f069fe0b29
Kartikey Gupta <145404457+Kartikey2203@users.noreply.github.com>
2026-08-16 06:38:15 +0530

    Time: 1 ms (6.39%), Space: 8.5 MB (39.04%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -9,9 +9,23 @@ public:
         }
         return memo[n];
     }
+    int tab(int n,vector<int>& dp){
+        if ( n == 2 || n==1) {
+            return n;
+        }
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3; i<=n; i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+
+    }
 
     int climbStairs(int n) {
         unordered_map<int, int> memo;
-        return climbStairs(n, memo);
+        vector<int> dp(n+1,0);
+        return tab(n,dp);
+        // return climbStairs(n, memo);
     }
 };
