952e3d7eea8a07ff0a00b8f4017df807567cce52
Kartikey Gupta <145404457+Kartikey2203@users.noreply.github.com>
2026-08-16 06:52:37 +0530

    Time: 0 ms (100%), Space: 10.7 MB (47.17%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
-    int sol_mem(vector<int>& nums,vector<int>& dp,int i){
-        // if(i>=nums.size()){
-        //     return 0;
-        // }
-        // if(dp[i]!=0){
-        //     return dp[i];
-        // }
-        int ans=0;
-        // dp[i-1]=nums[i-1];
-        // dp[1]=nums[1];
-        // if(i+1<nums.size())
-        for(int j=i-1; j>=0; j--){
-         dp[j]=max(nums[j]+dp[j+2],dp[j+1]);
+    int mem(vector<int>& nums,int ind,vector<int>& dp){
+        if(ind >=nums.size()){
+            return 0;
         }
-        return dp[0];
+        if(dp[ind]!=-1){
+            return dp[ind];
+        }
+        int inc=nums[ind]+mem(nums,ind+2,dp);
+        int exc=mem(nums,ind+1,dp);
+        dp[ind]= max(inc,exc);
+        return dp[ind];
     }
     int rob(vector<int>& nums) {
-        int ans=0;
-        int n=nums.size();
-        vector<int>dp(n+2,0);
-
-        ans=sol_mem(nums,dp,n);
-        return ans;
+        vector<int> dp(nums.size()+1,-1);
+        return mem(nums,0,dp);
     }
 };
